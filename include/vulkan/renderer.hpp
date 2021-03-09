8f0b15ae9a9cc2101284c94b53831db0c432140e
retro <alexski1029@gmail.com>
2021-03-08 23:59:37 -0500

    Things can render

--- include/vulkan/renderer.hpp
+++ include/vulkan/renderer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vector>
 #include "window.hpp"
 #include "pxPhysicalDevices.hpp"
 #include "layers.hpp"
@@ -14,16 +15,38 @@ namespace plxVulkan{
         void cleanup();
     private:
         window* windowInstance;
+        VkSurfaceKHR surface;
+
         VkInstance instance;
+        layers layer;
         pxPhysicalDevices physicalDevices;
         VkDevice device;
+
         VkQueue graphicsQueue;
         VkQueue presentQueue;
-        VkSurfaceKHR surface;
+
         VkSwapchainKHR swapchain;
-        layers layer;
+        std::vector<VkImage> swapchainImages;
+        std::vector<VkImageView> swapchainImageViews;
+        VkFormat swapchainFormat;
+        VkExtent2D swapchainExtent;
+
+        VkPipelineLayout pipelineLayout;
+        VkRenderPass renderPass;
+        VkPipeline graphicsPipeline;
+
+        std::vector<VkFramebuffer> swapChainFramebuffers;
+        VkCommandPool commandPool;
+        std::vector<VkCommandBuffer> commandBuffers;
+
+        std::vector<VkSemaphore> imageAvailableSemaphores;
+        std::vector<VkSemaphore> renderFinishedSemaphores;
+        std::vector<VkFence> inFlightFences;
+        std::vector<VkFence> imagesInFlight;
+        size_t currentFrame = 0;
 
         void createInstance();
+        void DrawFrame();
         void initVulkan();
         void mainLoop();
         void CreateSurface();
@@ -31,5 +54,12 @@ namespace plxVulkan{
         void createLogicalDevice();
         void SetupVulkanDebugger();
         void CreateSwapchain();
+        void CreateImageViews();
+        void CreateGraphicsPipeline();
+        void CreateRenderPass();
+        void CreateFrameBuffers();
+        void CreateCommandPool();
+        void CreateCommandBuffers();
+        void CreateSyncObjects();
     };
 }
