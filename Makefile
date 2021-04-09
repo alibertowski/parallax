788d14c1fcd8cc149575a7952ed6a9e5fa923b2b
retro <alexski1029@gmail.com>
2021-04-08 21:43:30 -0400

    A lot of general stuff

--- Makefile
+++ Makefile
@@ -5,14 +5,20 @@ CFLAGS = -std=c++17 -Wall -Wextra -iquote ./include
 LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
 
 parallax: src/main.cpp
+	mkdir -p ./bin
+	
 	g++ $(CFLAGS) -g -o ${BINARY-FOLDER}/${EXECUTABLE-NAME} $(LDFLAGS) ./src/*.cpp ./src/vulkan/*.cpp
 	
 .PHONY: test clean
 
 release:
+	mkdir -p ./bin
+
 	g++ $(CFLAGS) -O3 -D NDEBUG -o ${BINARY-FOLDER}/${EXECUTABLE-NAME} $(LDFLAGS) ./src/*.cpp ./src/vulkan/*.cpp
 
 shaders:
+	mkdir -p ./bin/shaders
+
 	glslangValidator -V -o ${BINARY-FOLDER}/shaders/shaders.frag.spv shaders/shaders.frag
 	glslangValidator -V -o ${BINARY-FOLDER}/shaders/shaders.vert.spv shaders/shaders.vert
 .PHONY: shaders
--- src/window.cpp
+++ src/window.cpp
@@ -1,5 +1,6 @@
 #include "window.hpp"
 #include "settings.hpp"
+#include "input.hpp"
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -10,10 +11,30 @@ static void error_callback(int error, const char* description){
     std::cerr << "Error Code: " << error << ", Description: " << description << '\n';
 }
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if(action == GLFW_PRESS){
+        switch(key){
+            case Input::Keycode::One:
+                std::cout << "One\n";
+                break;
+            case Input::Keycode::Two:
+                std::cout << "Two\n";
+                break;
+            case Input::Keycode::Three:
+                std::cout << "Three\n";
+                break;
+            case Input::Keycode::Four:
+                std::cout << "Four\n";
+                break;
+        }
+    }
+}
+
 void window::window_init(){
     glfwSetErrorCallback(error_callback);
     if (!glfwInit()){
-        throw std::runtime_error{ "GLFW failed to initialize" };
+        throw std::runtime_error{ "GLFW failed to initialize" };    
     }
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -23,6 +44,8 @@ void window::window_init(){
         glfwTerminate();
         throw std::runtime_error("GLFW failed to create window");
     }
+
+    glfwSetKeyCallback(window, key_callback);
 }
 
 void window::cleanup(){
