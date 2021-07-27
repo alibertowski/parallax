57de656871ad3ca17dc444eae39705ecd5442756
retro <alexski1029@gmail.com>
2021-07-27 18:42:46 -0400

    Added shader classes and triangle OpenGL code

--- src/window.cpp
+++ src/window.cpp
@@ -1,5 +1,6 @@
 #include "window.hpp"
 #include "settings.hpp"
+#include "shader.hpp"
 #include "glad/glad.h"
 
 #define GLFW_INCLUDE_NONE
@@ -51,20 +52,90 @@ Window::Window() {
         throw std::runtime_error("GLAD failed to initialize");
     }
 
+    Shader vertexShader{ "shader.vert" };
+    Shader fragShader{ "shader.frag" };
+
+    auto v{vertexShader.get_source_code().c_str()};
+    auto f{ fragShader.get_source_code().c_str()};
+
+    GLuint vertexShaderGL{ glCreateShader(GL_VERTEX_SHADER) };
+    glShaderSource(vertexShaderGL, 1, &v, nullptr);
+    glCompileShader(vertexShaderGL);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShaderGL, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShaderGL, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &f, nullptr);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShaderGL);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShaderGL);
+    glDeleteShader(fragmentShader);
+    
+    float vertices[] = {
+        -0.2f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };  
+
+    GLuint vbo, vao;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
     while(!glfwWindowShouldClose(window)) {
         glClearColor(1.0f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-        
-        #ifndef NDEBUG
-        // Gets the deltatime and prints the FPS
-        double deltaTime{ glfwGetTime() };
-        glfwSetTime(0.0);
-      //  std::cout << 1 / deltaTime << '\n';
-        #endif
     }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(shaderProgram);
 }
 
 Window::~Window() {
