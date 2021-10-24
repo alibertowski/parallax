97e2bf8f0f6e3399ca86361c1770d9ab08f81eba
DominoGhost <57733758+DominoGhost@users.noreply.github.com>
2021-10-23 23:45:31 -0400

    Files to be pushed for a friend

--- include/camera.hpp
+++ include/camera.hpp
@@ -3,18 +3,25 @@
 #include "component.hpp"
 #include "settings.hpp"
 #include "coordinate_frame.hpp"
+#include "collision.hpp"
 
 #include <glm/mat4x4.hpp>
+#include <glm/ext/vector_float3.hpp>
 
 class Camera : public Component {
 public:
+    Camera();
+
     glm::mat4 calculate_view();
     glm::mat4 get_projection();
     virtual void update() override;
+    inline CoordinateFrame& get_coordinate_frame() { return cframe; };
+    inline const Collision& get_collision() const { return collision; };
 private:
     float lastX{ Settings::DefaultWidth / 2 };
     float lastY{ Settings::DefaultHeight / 2} ;
     float yaw{ -90.0f };
     float pitch{ 0.0f };
     CoordinateFrame cframe;
+    Collision collision{glm::vec3{ 1.0f, 3.0f, 1.0f} };
 };
--- src/engine.cpp
+++ src/engine.cpp
@@ -4,6 +4,7 @@
 #include "glfw_service.hpp"
 #include "component_manager.hpp"
 #include "time.hpp"
+#include "collision.hpp"
 
 #include <iostream>
 #include <string>
@@ -13,6 +14,20 @@ Engine* Engine::PrimaryEngine{ nullptr };
 Engine::Engine() {
     cam.set_name(std::string{ "Camera" });
     ComponentManager::add_component(&cam);
+}
+
+static bool Colliding(const Collision& a, const Collision& b) {
+    glm::vec3 minVec{a.get_min()};
+    glm::vec3 maxVec{a.get_max()};
+    glm::vec3 min2Vec{b.get_min()};
+    glm::vec3 max2Vec{b.get_max()};
+
+    return maxVec.x >= min2Vec.x &&
+            max2Vec.x >= minVec.x &&
+            maxVec.y >= min2Vec.y &&
+            max2Vec.y >= minVec.y &&
+            maxVec.z >= min2Vec.z &&
+            max2Vec.z >= minVec.z; // TODO: fix
 }
 
 void Engine::run() {
@@ -27,6 +42,9 @@ void Engine::run() {
         for(Component* comp : components) {
             comp->update();
         }
+
+//        cam.get_coordinate_frame().set_position(cam.get_coordinate_frame().get_position() - glm::vec3{0.0f, 2.0f * Time::get_delta_time(), 0.0f});
+        std::cout << Colliding(Collision{ glm::vec3{16.0f, 1.0f, 10.0f} }, cam.get_collision()) << '\n';
 
         r.render_frame();
         glfwSwapBuffers(primaryWindow);
