b82ff3a66324fa6cee38bc0bc6a98b537f587e0b
DominoGhost <57733758+DominoGhost@users.noreply.github.com>
2022-09-23 00:09:00 -0400

    Added GLFW

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+    std::cout << "hssdsdelo" << std::endl;
+    if (!glfwInit())
+        return 1;
+
+    glfwTerminate();
+    return 0;
+}
